14e4aef96855f272bef4a76d2a6092aa96393cbe
Jessica Weinstein <jessicaweinstein@Jessicas-MacBook-Pro.local>
2020-07-21 14:42:09 -0400

    google books info displaying title, author and book image for the first result [0] to the screen

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+$(document).ready(function (){
+
 const proxyurl = "https://cors-anywhere.herokuapp.com/"; 
-//goodReads was too hard to figure out the xml formatting, I couldn't find the results
+//this was using goodReads but too hard to figure out the xml formatting, I couldn't find the results
+//const url = "https://www.goodreads.com/search/index.xml?key=YOUR_KEY&q=Normal+People";
 //google books api 
-const url = "https://www.googleapis.com/books/v1/volumes?q=intitle:Normal+People+inauthor:Sally+Rooney"
+//const url = "https://www.googleapis.com/books/v1/volumes?q=intitle:Normal+People+inauthor:Sally+Rooney"
+const url = "https://www.googleapis.com/books/v1/volumes?q="
+
+let searchInput = document.getElementById("searchInput");
+
+let submit = document.getElementById("searchButton");
+    submit.addEventListener("click", searchBooksAPI);
+
+let bookTitle = document.getElementById("bookTitle");
+let author = document.getElementById("author");
+let year = document.getElementById("year");
+let coverImage = document.getElementById("coverImage");
 
 
+function searchBooksAPI(){
 $.ajax({
-    url: proxyurl + url,
-    //beforeSend: function(xhr) {
-         //xhr.setRequestHeader("Authorization", "Bearer 6QXNMEMFHNY4FJ5ELNFMP5KRW52WFXN5")
-    //}, 
-    success: function(data){
-        console.log(data);
+    url: proxyurl + url + searchInput.value,
+    //url: proxyurl + url,
+    success: function(response){
+        console.log(response);
         //process the JSON data etc
+        displayFirstResult(response)
     }
+})
+
+}
+
+function displayFirstResult(data){
+    bookTitle.innerHTML = "Book Title: " + data.items[0].volumeInfo.title
+    author.innerHTML = "Author: " + data.items[0].volumeInfo.authors[0]
+    coverImage.style.backgroundImage = "url('" + data.items[0].volumeInfo.imageLinks.thumbnail + "')"
+}
+
 })
